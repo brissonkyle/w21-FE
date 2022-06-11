e2e4366f4b3f9a85bf69c83f1b935b9181ec159e
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-11 13:11:32 -0600

    Pushing test Code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,63 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input v-model="animalName" type="text" id='animalInput'>
+    <input v-model="imageUrl" type="text" id='imageInput'>
+    <button @click="postAnimal">POST</button>
+    <button @click="getAnimals">GET</button>
+    <div v-for="animal in animalArr" :key="animal.animalId">
+      {{ animal.animalId }}
+      <h1>{{ animal.name }}</h1>
+      <img :src="animal.imageUrl">
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      animalName: null,
+      imageUrl : null
+    }
+  },
+  methods: {
+    getAnimals() {
+      console.log('RUNNING GET');
+      axios.request({
+        method : "GET",
+        url : 'http://127.0.0.1:5000/api/animals'
+      }).then((response)=> {
+        console.log(response);
+        this.animalArr = response.data;
+      }).catch(()=> {
+        
+      })
+    },
+    postAnimal() {
+      console.log('RUNNING POST');
+      axios.request({
+        method : "POST",
+        url : 'http://127.0.0.1:5000/api/animals',
+
+        data() {
+          return {
+            animalName: this.animalName,
+            imageUrl : this.imageUrl
+          }
+        },
+      }).then((response)=> {
+        console.log(response);
+        this.getAnimals
+      }).catch((error)=> {
+        console.log(error);
+      })
+    },
+    mounted () {
+      this.getAnimals();
+    },
+  },
 }
 </script>
 
